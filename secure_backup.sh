70f5f1dc8870fccb72c70ea293716612be678997
carbonyle elynobrac <carbonyle@users.noreply.github.com>
2015-06-29 21:30:44 +0200

    updated to use sha-256 instead of md5
    - sha256
    - relative paths

--- secure_backup.sh
+++ secure_backup.sh
@@ -3,72 +3,62 @@
 #  backup script
 #
 #
-#  Created by carbonyle on 15.06.15.
+#  Created by Gaëtan Cherbuin on 15.06.15.
 #
-
 
 #prepare gpg
 
-pkill gpg-agent >> /dev/null
-export SSH_AUTH_SOCK=~/.gnupg/S.gpg-agent.ssh >> /dev/null
-gpg-connect-agent "getinfo ssh_socket_name" /bye >> /dev/null
-gpg-agent --use-standard-socket --daemon >> /dev/null
+if system_profiler SPUSBDataType | grep -q Yubikey;
+then
+pkill gpg-agent
+export SSH_AUTH_SOCK=~/.gnupg/S.gpg-agent.ssh
+gpg-connect-agent "getinfo ssh_socket_name" /bye
+gpg-agent --use-standard-socket --daemon 
 clear
+
+else
+echo "Insert Yubikey & run again" && exit 1
+fi
 
 #backup
 
-if [ -f /Users/carbonyle/Backup/"$(date '+%d.%b.%Y').tar.gz.gpg" ];
+if [ -f ~/Backup/"$(date '+%d.%b.%Y').tar.gz.gpg" ];
 then
    echo "Backup already performed, try later" && exit 1
 else
 
-    read -r -p "Do you want to backup something else? (Y/n) " morebackup
-
-        if [[ $morebackup =~ ^([yY])$ ]]
-
-        then
-
-        read -r -p "Specify path to backup" pathtobackup
-        tar -zcf /Users/carbonyle/Backup/"$(date '+%d.%b.%Y').tar.gz" /Users/carbonyle/Documents/Job $pathtobackup
-
-        else
-
-        tar -zcf /Users/carbonyle/Backup/"$(date '+%d.%b.%Y').tar.gz" /Users/carbonyle/Documents/Job
-
-        fi
-
-fi
+tar -zcf ~/Backup/"$(date '+%d.%b.%Y').tar.gz" ~/Documents ~/OtherFoldersToBackup
 
 #test
 
-touch /Users/carbonyle/Backup/"$(date '+%d.%b.%Y').log"
-tar -ztvvf /Users/carbonyle/Backup/"$(date '+%d.%b.%Y').tar.gz" >> /Users/carbonyle/Backup/"$(date '+%d.%b.%Y').log"
+touch ~/Backup/"$(date '+%d.%b.%Y').log"
+tar -ztvvf ~/Backup/"$(date '+%d.%b.%Y').tar.gz" >> ~/Backup/"$(date '+%d.%b.%Y').log"
 clear
 
 #encrypt
 
-gpg -r carbonyle -e /Users/carbonyle/Backup/"$(date '+%d.%b.%Y').tar.gz"
-rm /Users/carbonyle/Backup/"$(date '+%d.%b.%Y').tar.gz"
+gpg -r carbonyle -e ~/Backup/"$(date '+%d.%b.%Y').tar.gz"
+rm ~/Backup/"$(date '+%d.%b.%Y').tar.gz"
 
-#generate md5
+#generate sha-256
 
-if [ -f /Users/carbonyle/Backup/md5.log ];
+if [ -f ~/Backup/sha-256.log ];
 then
-   md5 /Users/carbonyle/Backup/"$(date '+%d.%b.%Y').tar.gz.gpg" >> /Users/carbonyle/Backup/md5.log
+   shasum -a 256 ~/Backup/"$(date '+%d.%b.%Y').tar.gz.gpg" >> ~/Backup/sha-256.log
 else
-   touch /Users/carbonyle/Backup/md5.log && md5 /Users/carbonyle/Backup/"$(date '+%d.%b.%Y').tar.gz.gpg" >> /Users/carbonyle/Backup/md5.log
+   touch ~/Backup/md5.log && shasum -a 256 ~/Backup/"$(date '+%d.%b.%Y').tar.gz.gpg" >> ~/Backup/sha-256.log
 fi
 
 #sync to server
 
 read -r -p "Upload to [P]rism or to [c]arbonyle.net? " response
 
-if [[ $response =~ ^([cC])$ ]]
+if [[ $response =~ ^([cC])$ ]];
 then
-/Users/carbonyle/Desktop/rsync_3.1.2dev -avhzP -e "ssh -p 4321" /Users/carbonyle/Backup carbonyle@carbonyle.net:/home/carbonyle/Documents/
+~/Desktop/rsync_3.1.2dev -avhzP -e "ssh -p 4321" ~/Backup carbonyle@carbonyle.net:/home/carbonyle/Documents/
 
 else
-/Users/carbonyle/Desktop/rsync_3.1.2dev -avhzP -e "ssh -p 4321" /Users/carbonyle/Backup carbonyle@192.168.1.116:/home/carbonyle/Documents/
+~/Desktop/rsync_3.1.2dev -avhzP -e "ssh -p 4321" ~/Backup carbonyle@LocalPath
 
 fi
 
@@ -76,6 +66,6 @@ fi
 
 ssh -t carbonyle 'rm /home/carbonyle/Documents/Backup/check.log && /home/carbonyle/Documents/Backup/check && cat /home/carbonyle/Documents/Backup/check.log && exit'
 
-#cat local md5
+#cat local sha-256
 
-cat /Users/carbonyle/Backup/md5.log
+cat ~/Backup/sha-256.log
